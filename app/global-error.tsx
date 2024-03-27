d0a6557817b4cb82d82a96b237b0a378e41a8770
Uwancha <sito2alemu@gmail.com>
2024-03-28 02:03:24 +0300

    feat: Add GlobalError page component to catch error's that occurs in RootLayout

--- app/global-error.tsx
+++ app/global-error.tsx
@@ -1,1 +1,25 @@
+'use client'
+
+import Link from "next/link"
+
  
+export default function GlobalError({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string }
+  reset: () => void
+}) {
+  return (
+    <html>
+      <body>
+      <div className="flex justify-between px-8 py-8">
+        <Link href={'/'} className="text-primary text-2xl font-bold">MP</Link> 
+        <Link href={'/register'} className='text-2xl text-primary font-bold'>X</Link> 
+      </div>
+        <h2>Something went wrong!</h2>
+        <button onClick={() => reset()}>Try again</button>
+      </body>
+    </html>
+  )
+}
