94139a2b3e84231e91e86a922ab95ce900f845f7
Uwancha <sito2alemu@gmail.com>
2024-03-28 01:55:23 +0300

    feat: Add component to create a comment in a post

---
+++ app/forms/addComment.tsx
@@ -0,0 +1,27 @@
+'use client'
+
+import { CreateComment } from "@/app/actions/createcomment";
+import { useFormState } from "react-dom";
+
+// Component to create a comment in a post
+export default function AddComment({ postid } :{ postid: string} ) {
+    // Form state and server action to handle comment creation
+    const [formState, action] = useFormState(CreateComment, { message: '' });
+
+    return (
+        <div className="mx-8 sm:w-3/4 lg:w-1/2 sm:mx-auto">
+            <form action={action} className='w-full flex flex-col gap-2'>
+                <legend className='text-xl font-semi-bold'>Add Comment</legend>
+                <input hidden name="postId" value={postid} readOnly/>
+                <label >
+                    <textarea name="message" required className='w-full bg-inputbg font-light px-4 py-2 rounded-lg
+                        focus:outline-none focus:ring-1 focus:ring-primary'
+                    />
+                </label>
+                {/* Display any error that occurs during comment creation */}
+                <p>{formState?.message}</p>
+                <button type="submit" className='w-1/4 btn-shadow rounded-lg text-primary py-2'>Submit</button>
+            </form>
+        </div>
+    )
+};
