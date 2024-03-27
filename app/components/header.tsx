46309b7a518fec7be8cd90e9abc93a9ea2db08d2
Uwancha <sito2alemu@gmail.com>
2024-03-27 23:57:28 +0300

    feat: Implement conditional rendering in Header component based on user authentication
    Conditionally render navigation links based on user authentication status. Retrieve user information from cookies and render appropriate links accordingly.

---
+++ app/components/header.tsx
@@ -0,0 +1,30 @@
+import { cookies } from "next/headers";
+import Link from "next/link";
+import Nav from "./nav";
+
+export default function Header() {
+    // Retrieve user information from cookies
+    const cookieStore = cookies()
+    const user = cookieStore.get('user')?.value
+    const userId = cookieStore.get('userid')?.value
+
+    return (
+        <div className="flex justify-between px-8 py-4 shadow-sm">
+            {/* Brand logo */}
+            <Link href={'/'} className="text-primary text-2xl font-bold">MP</Link>
+
+            {/* Conditional rendering based on user authentication */} 
+            {userId ?
+            (
+                <div className="flex items-center gap-4">
+                    <Nav user={user} id={userId} />
+                </div>
+            ) : (
+                <div className="flex items-center gap-8 text-primary">
+                    <Link href={'/login'} className="" >Login</Link>
+                    <Link href={'/register'} >Register</Link>
+                </div>
+            )}
+        </div>
+    )
+}
