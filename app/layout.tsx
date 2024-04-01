4e4dfdf55ba721b1c16ed822b58c04c5d78f87dc
Uwancha <sito2alemu@gmail.com>
2024-04-01 03:27:18 +0300

    ui: Change font weight of RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
 import "./globals.css";
 
-const roboto = Roboto( { subsets: ["cyrillic"], weight: "400"} );
+const roboto = Roboto( { subsets: ["cyrillic"], weight: "300"} );
 
 export const metadata: Metadata = {
   title: "MP Blog | Home",
